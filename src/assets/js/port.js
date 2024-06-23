d5629177b92a7eb4f4c9976da64bf5be5c883444
unknown <shinlee7878@gmail.com>
2024-06-23 21:33:46 +0900

    😳 마무리 완료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,27 +1,3 @@
-// main.js
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-gsap.registerPlugin(ScrollTrigger);
-
 export function port() {
-    const horSection = gsap.utils.toArray(".port__item");
-
-    gsap.to(horSection, {
-        xPercent: -100 * (horSection.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: "#port",
-            start: "top top",
-            end: () => "+=" + document.querySelector("#port").offsetWidth,
-            pin: true,
-            scrub: 1,
-            markers: false,
-            invalidateOnRefresh: true,
-            anticipatePin: 1,
-        },
-    });
+    console.log(port)
 }
-
-// 실행
-port();
--- src/assets/js/smooth.js
+++ src/assets/js/smooth.js
@@ -1,3 +1,33 @@
+import Lenis from "@studio-freight/lenis";
+
 export function smooth() {
-    console.log(smooth)
+    const lenis = new Lenis({ 
+        duration: 1,
+        easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
+
+    function raf(time) {
+        lenis.raf(time);
+        requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    lenis.on("scroll", (e) => {
+        console.log(e);
+    });
+}
+export function link() {
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+        anchor.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute("href");
+            const targetElement = document.querySelector(targetId);
+
+            if (targetElement) {
+                targetElement.scrollIntoView({ behavior: "smooth" });
+            }
+        });
+    });
 }
