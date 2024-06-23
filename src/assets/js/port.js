4fdad30d50f47f2fa879e2161c61c66a3dd2e2bb
unknown <shinlee7878@gmail.com>
2024-06-23 21:21:27 +0900

    🤓 포트폴리오 완료

--- src/assets/js/port.js
+++ src/assets/js/port.js
@@ -1,3 +1,27 @@
+// main.js
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export function port() {
-    console.log(port)
+    const horSection = gsap.utils.toArray(".port__item");
+
+    gsap.to(horSection, {
+        xPercent: -100 * (horSection.length - 1),
+        ease: "none",
+        scrollTrigger: {
+            trigger: "#port",
+            start: "top top",
+            end: () => "+=" + document.querySelector("#port").offsetWidth,
+            pin: true,
+            scrub: 1,
+            markers: false,
+            invalidateOnRefresh: true,
+            anticipatePin: 1,
+        },
+    });
 }
+
+// 실행
+port();
