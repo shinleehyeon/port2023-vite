1eb2dd05a75cfb61ae7357cb97816b87ddc16fd1
unknown <shinlee7878@gmail.com>
2024-06-24 00:08:53 +0900

    최종본

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -9,26 +9,3 @@ window.addEventListener("load", function () {
     menu();
     port();
 });
-
-import gsap from "gsap";
-import ScrollTrigger from "gsap/ScrollTrigger";
-
-export function port() {
-    gsap.registerPlugin(ScrollTrigger);
-    const horSection = gsap.utils.toArray(".port__item");
-
-    gsap.to(horSection, {
-        xPercent: -120 * (horSection.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: "#port",
-            start: "top 56px",
-            end: "+=3000",
-            pin: true,
-            scrub: 1,
-            markers: false,
-            invalidateOnRefresh: true,
-            anticipatePin: 1,
-        },
-    });
-}
--- src/assets/js/menu.js
+++ src/assets/js/menu.js
@@ -1,11 +1,14 @@
-const headerToggle = document.getElementById("headerToggle");
-const headerNav = document.querySelector(".header__nav");
+export function menu(){
+    const headerToggle = document.getElementById("headerToggle");
+    const headerNav = document.querySelector(".header__nav");
 
-if (headerToggle) {
-    headerToggle.addEventListener("click", () => {
-    headerNav.classList.toggle("show");
-    headerToggle.getAttribute("aria-expanded") === "true"
-        ? headerToggle.setAttribute("aria-expanded", "false")
-        : headerToggle.setAttribute("aria-expanded", "true");
-    });
+    if(headerToggle){
+        headerToggle.addEventListener("click", () => {
+            headerNav.classList.toggle("show");
+
+            console.log(headerToggle.getAttribute("aria-expanded") === "true" 
+            ? headerToggle.getAttribute("aria-expanded", "false") 
+            : headerToggle.getAttribute("aria-expanded", "true"))
+      });
+    }   
 }
